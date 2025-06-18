6469911bd7e49caf4b04497e9871f4baceb7f6f5
sanskar <sanskarvns19@gmail.com>
2025-06-18 18:47:00 +0530

    Email verify

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css'
+
+
+
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { AppContextProvider } from './context/AppContext.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
